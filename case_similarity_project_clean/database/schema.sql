de688d7715e13b5937fc78b8f2682b0e1d4ba2d4
AkshatDalwala00 <dalwalaakshat123@gmail.com>
2026-02-17 10:19:13 +0530

    Delete case_similarity_project_clean directory

--- case_similarity_project_clean/database/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS cases (
-    case_id TEXT PRIMARY KEY,
-    embedding TEXT, -- JSON serialized list of floats
-    text TEXT,
-    decision TEXT,
-    decision_reason TEXT,
-    case_source TEXT
-);
